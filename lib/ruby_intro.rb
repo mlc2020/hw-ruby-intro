d31311a57a470227074a12309c535eac75eea572
EC2 Default User <ec2-user@ip-172-31-47-183.us-west-2.compute.internal>
2021-02-10 04:51:28 +0000

    Commit for HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,13 +4,17 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  twoele = arr.max(2)
+  twoele.sum
 end
 
 def sum_to_n? arr, n
+  return false if arr.size <= 1
+  arr.combination(2).any? {|x,y| x+y == n}
   # YOUR CODE HERE
 end
 
@@ -18,18 +22,35 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  /^[^aeiou\W]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  /^(?:[01]*00|0)$/i.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize isbn, price
+    if isbn.empty? || price <= 0
+      raise ArgumentError
+    else
+      @isbn = isbn
+      @price = price
+    end
+  end
+  
+  def price_as_string
+    @price = "$%.2f" % [@price]
+  end
 end
